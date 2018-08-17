0079d46ceaed37d53dbc3f8562f52a3cff633afc
Hubert Mach <hubert.mach@ingservicespolska.pl>
2018-08-17 10:35:07 +0200

    virtualenv update

--- .niwia/bin/activate.fish
+++ .niwia/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/data/Github.com/Niwia/.niwia3"
+set -gx VIRTUAL_ENV "/data/Github.com/Niwia/.niwia"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
